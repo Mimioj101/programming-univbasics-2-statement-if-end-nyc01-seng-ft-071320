2f083171984c1bf95800da2adf5c0c565c9db402
Mimi Ojserkis <mimioj@gmail.com>
2020-07-01 13:50:48 +0000

    Automatically backed up by Learn

--- lib/if_else_end.rb
+++ lib/if_else_end.rb
@@ -1,8 +1,18 @@
-run_code_inside = false
-puts "Code before if...end
+# run_code_inside = false
+# puts "Code before if...end
 
-if run_code_inside
-  puts "code inside"
+# if run_code_inside
+#   puts "code inside"
+# end
+
+# puts "Code after if...end"
+
+current_time = Time.now
+
+current_time = current_time.to_i
+
+if current_time % 2 == 0
+  puts "Even!"
+else current_time == false
+  puts "Odd!"
 end
-
-puts "Code after if...end"
